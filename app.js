d425143b15a1f3c9378c417a53dcb6f98dd35e0f
rain <753342031@qq.com>
2017-07-18 21:19:39 +0800

    放开了端口

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var router = require('./router.js');
+var data = require('./data.js')
 var mountRenderForResponse = require('./context.js');
 var server = http.createServer(function (req, res) {
     mountRenderForResponse.setRander(req, res);
@@ -7,6 +8,6 @@ var server = http.createServer(function (req, res) {
     router(req, res);
 
 })
-server.listen(9090, function () {
-    console.log('http://localhost:9090');
+server.listen(data.port, function () {
+    console.log('http://localhost:'+data.port);
 });
--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var path = require("path");
 module.exports = {
-    "port": 9090,
+    "port": 8080,
     "dataPath": path.join(__dirname, 'data', 'data.json'),
     "viewsPath":path.join(__dirname, 'views')
 }
